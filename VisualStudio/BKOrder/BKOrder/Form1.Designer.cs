0934dfb5b73f1c6c39484b2848cf946f60f8f345
Christoffer Sundbom <christoffer_karlsson@live.se>
2023-11-14 15:54:14 +0100

    added vs and moved around

---
+++ VisualStudio/BKOrder/BKOrder/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BKOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            btnSearch = new Button();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 41);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(159, 394);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(118, 12);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(53, 23);
+            btnSearch.TabIndex = 2;
+            btnSearch.Text = "Sök";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(177, 41);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 3;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1350, 729);
+            Controls.Add(btnAdd);
+            Controls.Add(btnSearch);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "BK-Order";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button btnSearch;
+        private Button btnAdd;
+    }
+}
---
+++ VisualStudio/BKOrder/BKOrder/Form1.cs
@@ -0,0 +1,96 @@
+using OfficeOpenXml;
+using System.Data;
+using System.Diagnostics;
+using System.IO.Packaging;
+using System.Text;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BKOrder
+{
+    public partial class Form1 : Form
+    {
+        private List<(string, string, string)> excelData = new List<(string, string, string)>();
+
+        private Dictionary<string, string> listBoxMapping = new Dictionary<string, string>();
+        string excelPath = "C:\\Users\\Chrkar01\\Documents\\GitHub\\godot-excel-reader\\Beställningar 2023.xlsx";
+        public Form1() {
+            InitializeComponent();
+            Debug.WriteLine("Loading project");
+            LoadExcelData();
+
+            //LoadExcelDataToListBox(excelPath);
+        }
+
+        private void LoadExcelData() {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                string filePath = openFileDialog.FileName;
+
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath))) {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                    excelData.Clear(); // Clear the data list
+
+                    for (int rowNum = 2; rowNum <= worksheet.Dimension.End.Row; rowNum++) {
+                        string column1 = worksheet.Cells[rowNum, 1].Text;
+                        string column2 = worksheet.Cells[rowNum, 2].Text;
+                        string column6 = worksheet.Cells[rowNum, 6].Text;
+
+                        excelData.Add((column1, column2, column6));
+                    }
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e) {
+            string searchTerm = textBox1.Text.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(searchTerm)) {
+                // If the search term is empty or whitespace, show all items from excelData.
+                PopulateListBox(excelData.Select(item => item.Item2).ToList());
+            }
+            else {
+                // Filter excelData based on the search term.
+                var filteredItems = excelData
+                    .Where(item => item.Item2.ToLower().Contains(searchTerm))
+                    .Select(item => item.Item2)
+                    .ToList();
+
+                // Populate the ListBox with the filtered items.
+                PopulateListBox(filteredItems);
+            }
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            /*foreach ((string column1, string column2, string column6) in excelData) {
+                Debug.WriteLine($"Column1: {column1}, Column2: {column2}, Column6: {column6}");
+            }*/
+
+            string selectedItem = listBox1.SelectedItem as string;
+
+            if (selectedItem != null) {
+                var matchingRow = excelData.FirstOrDefault(item => item.Item2 == selectedItem);
+
+                if (matchingRow != default) {
+                    Debug.WriteLine($"Column1: {matchingRow.Item1}, Column2: {matchingRow.Item2}, Column6: {matchingRow.Item3}");
+                }
+            }
+        }
+
+        private void PopulateListBox(List<string> items) {
+            listBox1.Items.Clear();
+
+            foreach (string item in items) {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
+
